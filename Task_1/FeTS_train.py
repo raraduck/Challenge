5d863d785727b3dfe5b9ff2dcde520ff51bb69b3
dwnsnu <dwnsnu@snu.ac.kr>
2024-07-11 09:49:21 +0900

    VPID noramlize

--- Task_1/FeTS_train.py
+++ Task_1/FeTS_train.py
@@ -287,7 +287,9 @@ def main(argv, trg_folder, trg_path, brats_training_data_parent_dir):
             total_integ = sum(integ)
             integ = [el / total_integ for el in integ]
             
-            VPID = [0.2*w+0.1*m+0.7*k for (w, m, k) in zip(weight, integ, deriv)]
+            VPID = [s*(0.2*w+0.1*m+0.7*k) for (s, w, m, k) in zip(switch, weight, integ, deriv)]
+            total_VPID = sum(VPID) + 1e-10
+            VPID = [el/total_VPID for el in VPID]
 
             tensor_values = [t.tensor for t in local_tensors]
             return np.average(tensor_values, weights=VPID, axis=0)
