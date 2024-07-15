cfa7a3d51c4eefdb143dd5008559833f355b94e6
dwnsnu <dwnsnu@snu.ac.kr>
2024-07-15 20:22:47 +0900

    update

--- Task_1/FeTS_Challenge_FedPOD_partitioning2.py
+++ Task_1/FeTS_Challenge_FedPOD_partitioning2.py
@@ -76,15 +76,15 @@ def main(argv, trg_folder, trg_path, brats_training_data_parent_dir):
                 n_nodes = 15
         elif argv.institution_split_csv_filename == 'partitioning_2.csv':
             if fl_round >= 0:
-                n_nodes = 10
+                n_nodes = 6
             if fl_round >= 5:
-                n_nodes = 12
+                n_nodes = 8
             if fl_round >= 10:
-                n_nodes = 15
+                n_nodes = 10
             if fl_round >= 15:
-                n_nodes = 18
+                n_nodes = 12
             if fl_round >= 20:
-                n_nodes = 20
+                n_nodes = 15
         elif argv.institution_split_csv_filename == 'partitioning_3.csv':
             if fl_round >= 0:
                 n_nodes = 6
