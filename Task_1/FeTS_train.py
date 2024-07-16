bcbe51499e59c74e87f14fe5eb1ee366fdb44096
dwnusa-win10 <dwnusa@gmail.com>
2024-07-16 20:30:04 +0900

    update

--- Task_1/FeTS_train.py
+++ Task_1/FeTS_train.py
@@ -36,13 +36,15 @@ def main(argv, trg_folder, trg_path, brats_training_data_parent_dir):
     secondary_group = [int(el) for el in original_unique_IDs if el not in primary_group]
 
     for pid in original_x100_unique_IDs:
-        pid = int(pid)
         indices = df[df['Partition_ID'] == pid].index
-        # print(pid, [el*100 for el in primary_group], pid in [el*100 for el in primary_group], type(pid))
+        pid = int(pid)
+        print(pid, [el*100 for el in primary_group], pid in [el*100 for el in primary_group], type(pid))
         if pid in [el*100 for el in primary_group]:
             df.loc[indices, 'Partition_ID'] = [str(int(pid) + i // subset_size) for i in range(len(indices))]
     # exit(0)
     subset_x100_unique_IDs = df['Partition_ID'].unique()
+    print(subset_x100_unique_IDs)
+    exit(0)
     # print(f"subset_x100_unique_IDs({len(subset_x100_unique_IDs)}): {subset_x100_unique_IDs}")
 
     # print(f"secondary_group({len(secondary_group)}): {secondary_group}")
